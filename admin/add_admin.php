8676c46c1322f952957123482db52556c730c6a3
Mico <micogianan28@gmail.com>
2024-02-17 19:19:25 +0800

    init

---
+++ admin/add_admin.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("../connection/connect.php");
+error_reporting(0);
+session_start();
+
+
+if(isset($_POST['submit']))          
+{
+	
+    if(empty($_POST['adm_name']) || 
+    empty($_POST['fname'])|| 
+ empty($_POST['lname'])||
+ empty($_POST['email']) ||  
+ empty($_POST['password'])||
+ empty($_POST['cpassword']) ||
+ empty($_POST['cpassword']))
+ {
+    $error = 	'<div class="alert alert-danger alert-dismissible fade show">
+																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+																<strong>All fields Must be Fillup!</strong>
+															</div>';
+ }
+	else
+		{
+		
+
+    $check_username= mysqli_query($db, "SELECT username FROM admin where username = '".$_POST['username']."' ");
+	$check_email = mysqli_query($db, "SELECT email FROM admin where email = '".$_POST['email']."' ");
+
+
+				$fname = $_FILES['file']['name'];
+								$temp = $_FILES['file']['tmp_name'];
+								$fsize = $_FILES['file']['size'];
+								$extension = explode('.',$fname);
+								$extension = strtolower(end($extension));  
+								$fnew = uniqid().'.'.$extension;
+   
+								$store = "Res_img/admin/".basename($fnew);                    
+	
+					if($extension == 'jpg'||$extension == 'png'||$extension == 'gif' )
+					{        
+									if($fsize>=10000000)
+										{
+		
+		
+												$error = 	'<div class="alert alert-danger alert-dismissible fade show">
+																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+																<strong>Too Big!!!</strong> Try different Image.
+															</div>';
+	   
+										}
+		
+
+                                     elseif($_POST['password'] != $_POST['cpassword']){  
+       	
+                                            echo "<script>alert('Password not match');</script>"; 
+                                      }
+                                      elseif(strlen($_POST['password']) < 6)  
+                                      {
+                                        echo "<script>alert('Password Must be >=6');</script>"; 
+                                      }
+    
+                                  
+                                      elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) 
+                                      {
+                                            echo "<script>alert('Invalid email address please type a valid email!');</script>"; 
+                                      }
+                                      elseif(mysqli_num_rows($check_username) > 0) 
+                                       {
+                                         echo "<script>alert('Username Already exists!');</script>"; 
+                                       }
+                                      elseif(mysqli_num_rows($check_email) > 0) 
+                                       {
+                                         echo "<script>alert('Email Already exists!');</script>"; 
+                                       }
+                                      else{
+												       
+												$sql = "INSERT INTO admin(username,f_name,l_name,password,email,img) VALUE('".$_POST['adm_name']."','".$_POST['fname']."','".$_POST['lname']."','".md5($_POST['password'])."','".$_POST['email']."','".$fnew."')";  // store the submited data ino the database :images
+												mysqli_query($db, $sql); 
+												move_uploaded_file($temp, $store);
+			  
+													$success = 	'<div class="alert alert-success alert-dismissible fade show">
+																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+																 New Admin Added Successfully.
+															</div>';
+                
+	
+										}
+					}
+					elseif($extension == '')
+					{
+						$error = 	'<div class="alert alert-danger alert-dismissible fade show">
+																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+																<strong>select image</strong>
+															</div>';
+					}
+					else{
+					
+											$error = 	'<div class="alert alert-danger alert-dismissible fade show">
+																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+																<strong>invalid extension!</strong>png, jpg, Gif are accepted.
+															</div>';
+						
+	   
+						}               
+	   
+	   
+	   }
+
+
+
+	
+	
+	
+
+}
+
+
+
+
+
+
+
+
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <title>Add Admin</title>
+    <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="css/helper.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body class="fix-header">
+  
+    <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+			<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
+    </div>
+  
+    <div id="main-wrapper">
+      
+         <div class="header">
+            <nav class="navbar top-navbar navbar-expand-md navbar-light">
+            <div class="navbar-header">
+                    <a class="navbar-brand" href="dashboard.php">
+                        
+                        <span><img src="images/icn.png" alt="homepage" class="dark-logo" /></span>
+                    </a>
+                </div>
+                <div class="navbar-collapse">
+       
+                    <ul class="navbar-nav mr-auto mt-md-0">
+              
+                        
+                     
+                       
+                    </ul>
+          
+                    <ul class="navbar-nav my-lg-0">
+
+                        
+          
+                        <li class="nav-item dropdown">
+                           
+                            <div class="dropdown-menu dropdown-menu-right mailbox animated zoomIn">
+                                <ul>
+                                    <li>
+                                        <div class="drop-title">Notifications</div>
+                                    </li>
+                                    
+                                    <li>
+                                        <a class="nav-link text-center" href="javascript:void(0);"> <strong>Check all notifications</strong> <i class="fa fa-angle-right"></i> </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                  
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-muted  " href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="images/bookingSystem/user-icn.png" alt="user" class="profile-pic" /></a>
+                            <div class="dropdown-menu dropdown-menu-right animated zoomIn">
+                                <ul class="dropdown-user">
+                                   <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+     
+        <div class="left-sidebar">
+     
+            <div class="scroll-sidebar">
+               
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li class="nav-devider"></li>
+                        <li class="nav-label">Home</li>
+                        <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Dashboard</span></a></li>
+                        <li class="nav-label">Log</li>
+                        <li> <a href="all_users.php">  <span><i class="fa fa-user f-s-20 "></i></span><span>Users</span></a></li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i><span class="hide-menu">Admins</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="all_admin.php">All Admin</a></li>
+								<li><a href="add_admin.php">Add Admin</a></li>
+                                </ul>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i><span class="hide-menu">Admins</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="all_admin.php">All Admin</a></li>
+								<li><a href="add_admin.php">Add Admin</a></li>
+                                </ul>
+                        
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-archive f-s-20 color-warning"></i><span class="hide-menu">Branch</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="all_restaurant.php">All Branch</a></li>
+								<li><a href="add_category.php">Add Category</a></li>
+                                <li><a href="add_restaurant.php">Add Branch</a></li>
+                                
+                            </ul>
+                        </li>
+                      <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery" aria-hidden="true"></i><span class="hide-menu">Menu</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="all_menu.php">All Menues</a></li>
+								<li><a href="add_menu.php">Add Menu</a></li>
+                              
+                                
+                            </ul>
+                        </li>
+						 <li> <a href="all_orders.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Orders</span></a></li>
+                         
+                    </ul>
+                </nav>
+            
+            </div>
+        
+        </div>
+      
+        <div class="page-wrapper">
+     
+            
+         
+            <div class="container-fluid">
+                <!-- Start Page Content -->
+                  
+									
+									<?php  echo $error;
+									        echo $success; ?>
+									
+									
+								
+								
+                                    <div class="col-lg-12">
+                        <div class="card card-outline-primary">
+                            <div class="card-header">
+                                <h4 class="m-b-0 text-white">Add Admin/Staff</h4>
+                            </div>
+                            <div class="card-body">
+                                <form action='' method='post'  enctype="multipart/form-data">
+                                    <div class="form-body">
+                                       
+                                        <hr>
+                                        <div class="row p-t-20">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="control-label">Username</label>
+                                                    <input type="text" name="adm_name" class="form-control" >
+                                                   </div>
+                                            </div>
+                                      
+                                            <div class="col-md-6">
+                                                <div class="form-group has-danger">
+                                                    <label class="control-label">First Name</label>
+                                                    <input type="text" name="fname" class="form-control form-control-danger" >
+                                                    </div>
+                                            </div>
+                                     
+                                        </div>
+                                  
+                                        <div class="row p-t-20">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="control-label">Last Name </label>
+                                                    <input type="text" name="lname" class="form-control">
+                                                   </div>
+                                            </div>
+                                   
+                                       
+                                    
+                                            <div class="form-group col-sm-6">
+                                             <label for="exampleInputEmail1">Email Address</label>
+                                             <input type="text" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp"> 
+                                            
+                                            </div>
+                                    
+                                           
+                              
+                                        </div>
+
+                                        
+                                    <div class="row p-t-20">
+                                        <div class="col-md-6">
+                                        <div class="form-group">
+                                       <label for="exampleInputPassword1">Password</label>
+                                       <input type="password" class="form-control" name="password" id="exampleInputPassword1"> 
+                                        </div>
+                                    
+                                    </div> 
+
+                                    <div class="form-group col-sm-6">
+                                       <label for="exampleInputPassword1">Confirm password</label>
+                                       <input type="password" class="form-control" name="cpassword" id="exampleInputPassword2"> 
+                                    </div>
+
+                                    </div> 
+											
+											
+                                    <div class="row p-t-20">		
+                                    <div class="col-md-6">
+                                                <div class="form-group has-danger">
+                                                    <label class="control-label">Image</label>
+                                                    <input type="file" name="file"  id="lastName" class="form-control form-control-danger" placeholder="12n">
+                                                    </div>
+                                            </div>
+                                        </div>
+											
+                                        </div>
+                                     
+                                        </div>
+                                    </div>
+                                    <div class="form-actions">
+                                        <input type="submit" name="submit" class="btn btn-primary" value="Register"> 
+                                        <a href="add_admin.php" class="btn btn-inverse">Cancel</a>
+                                    </div>
+                                </form>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <footer class="footer"> © 2023 - BurgerByte </footer>
+                </div>               
+            </div>
+        </div>    
+    </div>
+  
+    <script src="js/lib/jquery/jquery.min.js"></script>
+    <script src="js/lib/bootstrap/js/popper.min.js"></script>
+    <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/jquery.slimscroll.js"></script>
+    <script src="js/sidebarmenu.js"></script>
+    <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="js/custom.min.js"></script>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+
+</body>
+
+</html>
