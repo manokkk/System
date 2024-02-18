e65505a7979b09782874c142dee29a6b3bdc28bd
Mico <micogianan28@gmail.com>
2024-02-18 19:26:03 +0800

    change name db

--- connection/connect.php
+++ connection/connect.php
@@ -4,7 +4,7 @@
 $servername = "localhost"; //server
 $username = "root"; //username
 $password = ""; //password
-$dbname = "foods";  //database
+$dbname = "snipsnap";  //database
 
 // Create connection
 $db = mysqli_connect($servername, $username, $password, $dbname); // connecting 
