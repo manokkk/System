7c5874ca0b27ce1a96c5e50a03a721a8c6a08db5
Mico <micogianan28@gmail.com>
2024-02-26 13:36:13 +0800

    site logo

--- admin/view_order.php
+++ admin/view_order.php
@@ -46,7 +46,7 @@ popUpWin = open(URLStr,'popUpWin', 'toolbar=no,location=no,directories=no,status
             <div class="navbar-header">
                     <a class="navbar-brand" href="dashboard.php">
                         
-                        <span><img src="images/icn.png" alt="homepage" class="dark-logo" /></span>
+                        <span><img src="images/siteLogo.png" alt="homepage" class="dark-logo" /></span>
                     </a>
                 </div>
                 <div class="navbar-collapse">
